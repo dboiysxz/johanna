10f10611455c7079781873eb2efe3940536a233c
dboiysxz2 <dboiysxz@gmail.com>
2022-04-12 17:44:00 +0200

    adding commercial page

---
+++ commercial.php
@@ -0,0 +1,69 @@
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
+    <title>johanna ulfsak</title>
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="./css/commercial.css"/>
+    <script src="./script.js" defer></script>
+</head>
+<body>
+    <!-- ------------------------ NAVBAR ---------------------------------- -->
+    <section class="nav">
+            <div class="title-button">
+                <a href="./#about">johanna ulfsak</a>
+            </div>
+    </section>
+    <section class="grid-container">
+            <div class="grid-item">
+                <a href="img/Carpet 1_jpeg.jpg" data-lightbox="carpet image :)" data-caption="Caption1">
+                    <div class="grid-overlay"></div>
+                    <img src="img/Carpet 1_jpeg.jpg" alt="carpet3">
+                </a>
+            </div>
+            <div class="grid-item">
+                <a href="img/Carpet 1_jpeg.jpg" data-lightbox="carpet image :)" data-caption="Caption1">
+                    <div class="grid-overlay"></div>
+                    <img src="img/Carpet 1_jpeg.jpg" alt="carpet3">
+                </a>
+            </div>
+            <div class="grid-item">
+                <a href="img/Carpet 1_jpeg.jpg" data-lightbox="carpet image :)" data-caption="Caption1">
+                    <div class="grid-overlay"></div>
+                    <img src="img/Carpet 1_jpeg.jpg" alt="carpet3">
+                </a>
+            </div>
+            <div class="grid-item">
+                <a href="img/Carpet 1_jpeg.jpg" data-lightbox="carpet image :)" data-caption="Caption1">
+                    <div class="grid-overlay"></div>
+                    <img src="img/Carpet 1_jpeg.jpg" alt="carpet3">
+                </a>
+            </div>
+            <div class="grid-item">
+                <a href="img/Carpet 1_jpeg.jpg" data-lightbox="carpet image :)" data-caption="Caption1">
+                    <div class="grid-overlay"></div>
+                    <img src="img/Carpet 1_jpeg.jpg" alt="carpet3">
+                </a>
+            </div>
+            <div class="grid-item">
+                <a href="img/Carpet 1_jpeg.jpg" data-lightbox="carpet image :)" data-caption="Caption1">
+                    <div class="grid-overlay"></div>
+                    <img src="img/Carpet 1_jpeg.jpg" alt="carpet3">
+                </a>
+            </div>
+            <div class="grid-item">
+                <a href="img/Carpet 1_jpeg.jpg" data-lightbox="carpet image :)" data-caption="Caption1">
+                    <div class="grid-overlay"></div>
+                    <img src="img/Carpet 1_jpeg.jpg" alt="carpet3">
+                </a>
+            </div>
+    </section>
+</body>
+</html>
