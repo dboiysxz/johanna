c74c6753dcd91d90df32d8969f69fd843c143a72
dboiysxz2 <dboiysxz@gmail.com>
2022-04-15 21:41:41 +0200

    feat: ready.

--- web/static/assets/js/main.js
+++ web/static/assets/js/main.js
@@ -1,13 +1,20 @@
 jQuery(function() {
     var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 0,
+        slidesPerView: 'auto',
+        spaceBetween: 20,
         threshold: 4,
+        centeredSlides: true,
         navigation: {
             nextEl: '.carousel-button-right',
             prevEl: '.carousel-button-left',
         },
         loop: true,
+        breakpoints: {
+            992: {
+              slidesPerView: 1,
+              spaceBetween: 0
+            }
+        }
         // autoplay: {
         //     delay: 5000,
         //     pauseOnMouseEnter: true,
